c4bed2e32d8b8dbbaa701f59898af3a817ce41e7
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-18 14:00:22 +0300

    Update game.js file
    // Create an array to hold the sequence "red", "blue", "green", "yellow" .
    var buttonColours = ["red", "blue", "green", "yellow"];
    
    // Create a new empty array gamePattern to fiil it with random color.
    var gamePattern = [];
    
    // Add function to fill empty array with random color
    function nextSequence() {
    
      var randomNumber = Math.floor(Math.random() * 4);
    
      var randomChosenColour = buttonColours[randomNumber];
    
      gamePattern.push(randomChosenColour);
    
      console.log(gamePattern);
    
    };

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-alert('hhh');
+// Create an array to hold the sequence "red", "blue", "green", "yellow" .
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// Create a new empty array gamePattern to fiil it with random color.
+var gamePattern = [];
+
+// Add function to fill empty array with random color
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  console.log(gamePattern);
+
+};
