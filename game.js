926d53a10d5c72220d42bb9ddbbd72a2f820c88e
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-19 15:56:09 +0300

    Add new function playSound
    // Add function to play sound based on which button was pressed
    function playSound (name) {
       var audio = new Audio(name + ".mp3");
       audio.play();
    };

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // Create an array to hold the sequence "red", "blue", "green", "yellow" .
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
 
 // Create a new empty array gamePattern to fiil it with random color.
 var gamePattern = [];
@@ -12,23 +12,26 @@ function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
 
-  var randomChosenColour = buttonColours[randomNumber];
+  var randomChosenColor = buttonColors[randomNumber];
 
-  gamePattern.push(randomChosenColour);
+  gamePattern.push(randomChosenColor);
 
   // Use jQuery to animate a flash to the button selected 
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
 
-
-  // Play the sound for the button colour selected in step 1.
-
-  var audio = new Audio(randomChosenColour + ".mp3");
-  
-  audio.play();
 
   console.log(gamePattern);
+
+};
+
+// Add function to play sound based on which button was pressed
+function playSound (name) {
+   var audio = new Audio(name + ".mp3");
+  
+   audio.play();
+
 
 };
 
@@ -41,4 +44,8 @@ $('.btn').click(function(event){
    userClickedPattern.push(userChosenColor);
 
    console.log(userClickedPattern);
+
+   playSound(userChosenColor);
+
+
 });
