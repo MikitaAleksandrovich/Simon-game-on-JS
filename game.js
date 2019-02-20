de29c44b93eefae4fbf08e6e8df715b4742b4b4a
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-20 09:55:05 +0300

    End up with game
    End coding game and added final version of game.js

--- game.js
+++ game.js
@@ -7,8 +7,28 @@ var gamePattern = [];
 // Create a new empty array userClickedPattern to fiil it with clicked button's ids (colors). 
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function() {
+   if (!started) {
+ 
+     //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+     $("#level-title").text("Level " + level);
+     nextSequence();
+     started = true;
+   }
+ });
+
 // Add function to fill empty array with random color
 function nextSequence() {
+
+   userClickedPattern = [];
+
+  level++;
+
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
 
@@ -20,50 +40,63 @@ function nextSequence() {
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-
-
-  console.log(gamePattern);
-
+   playSound(randomChosenColor);
 };
+
+
+function checkAnswer (currentLevel) {
+   if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log('succes!');
+
+      if(userClickedPattern.length === gamePattern.length) {
+         setTimeout(function () {
+            nextSequence();
+         }, 1000);
+      }
+   } else {
+      playSound('wrong');
+      $('body').addClass("game-over");
+      setTimeout(function () {
+      $('body').removeClass("game-over");
+      }, 200);
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+   }
+}
 
 // Add function to play sound based on which button was pressed
 function playSound (name) {
    var audio = new Audio(name + ".mp3");
-  
    audio.play();
-
-
 };
+
 
 // Add function to animate button that was pressed
 function animatePress(currentColor) {
 
-  
    $("#" + currentColor).addClass("pressed");
 
-    
    setTimeout(function () {
-    
 	$("#" + currentColor).removeClass("pressed");
-  
    }, 100);
 
-}
-;
-
+};
 
 // Add function to define which button was clicked and put consistently it's ids into new array userClickedPattern
 $('.btn').click(function(event){
-
    var userChosenColor = $(this).attr('id');
-
    userClickedPattern.push(userChosenColor);
-
    console.log(userClickedPattern);
 
    playSound(userChosenColor);
-
-
-
    animatePress(userChosenColor);
+
+   checkAnswer(userClickedPattern.length - 1);
 });
+
+function startOver () {
+   level = 0;
+   gamePattern = [];
+   started = false;
+}
